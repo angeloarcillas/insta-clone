f4a46d3a5941aa316b9fe5493e747cb4845ec61e
angeloarcillas <angeloarcillas512@gmail.com>
2020-10-05 19:19:39 +0800

    Add tailwindcss | npm install && npm run dev

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -11,14 +12,17 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- use .extract() -> cli npm run dev -> include manifest/vendor/app --}}
+    <script src="{{ mix('js/manifest.js') }}" defer></script> {{-- public/vendor/mix-manifest --}}
+    <script src="{{ mix('js/vendor.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .extract() // extract all
-   .version(); // file version 
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/tailwind.css",
+    "public/css",
+    [require("tailwindcss")]
+);
